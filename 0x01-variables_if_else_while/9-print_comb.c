1164a4c503911ab831193a750ec4dcff8df2b847
Pho <pho.dmc5@gmail.com>
2023-07-15 00:03:12 +0000

    print all possible combination of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
+
 	return (0);
 }
