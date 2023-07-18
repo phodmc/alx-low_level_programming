e9f59dba1a28416f2bd52e63e25d3526a5ae9208
Pho <pho.dmc5@gmail.com>
2023-07-18 21:03:50 +0000

    checks for alphabetic chars

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/**
+* _isalpha - checks if c is an alphabetic character
+* @c: character to check
+*
+* Return: 1 if c is a letter, otherwise return 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
