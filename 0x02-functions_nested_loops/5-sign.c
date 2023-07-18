c2b5bc1e11c6a3b0f5de00d5291a373fcb4e6691
Pho <pho.dmc5@gmail.com>
2023-07-18 21:20:55 +0000

    check sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * print_sign - prints the sign of a number n
 * @n: number to be checked
@@ -9,14 +11,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
