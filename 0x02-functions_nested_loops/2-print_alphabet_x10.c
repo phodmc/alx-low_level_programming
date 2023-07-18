4b8eb15e8e77517d66d9e6dabaf545bd54853e56
Pho <pho.dmc5@gmail.com>
2023-07-18 17:16:08 +0000

    fix missing letter

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	while (counter < 10)
 	{
-		for (letter = 'a'; letter < 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
