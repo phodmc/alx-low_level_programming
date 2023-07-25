637677248fbc14fd3b9fd2ee9ee58b236f324422
mstrchf <phonum5@gmail.com>
2023-07-25 21:13:32 +0000

    add function that gets string length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,3 +17,25 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
+/**
+* _strlen - returns length of string s
+* @s: char pointer variable
+*
+* Return: int - lenght of string s
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (*s != '\0')
+	{
+		counter++;
+		s++;
+	}
+
+	return (counter);
+}
+
