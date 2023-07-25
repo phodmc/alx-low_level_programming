b01cbd8e2bba39abecc2e651434ffd669994b59a
mstrchf <phonum5@gmail.com>
2023-07-25 22:40:16 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string s
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int size, i, j;
+	char temp[_strlen(s)];
+
+	size = _strlen(s);
+	i = 0;
+	while (size > 0)
+	{
+		temp[i] = s[size - 1];
+		size--;
+		i++;
+	}
+
+	j = 0;
+	while( j < i)
+	{
+		s[j] = temp[j];
+		j++;
+	}	
+}	
+/**
+* _strlen - returns length of string s
+* @s: char pointer variable
+*
+* Return: int - lenght of string s
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (*s != '\0')
+	{
+		counter++;
+		s++;
+	}
+
+	return (counter);
+}
+
