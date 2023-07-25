bdcddcac420f7a2790b6fb93870ee9c47e4521f0
Pho <pho.dmc5@gmail.com>
2023-07-25 14:41:26 +0000

    return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strlen - returns length of string s
+* @s: char pointer variable
+*
+* Return: int - lenght of string s
+*/
+
+int _strlen(char *s);
+{
+	int counter;
+
+	while (*s != '\0')
+	{
+		counter++;
+		s++;
+	}
+
+	return (counter);
+}
