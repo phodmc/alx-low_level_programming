9ab496c54dce7504d72499c50b42845616db4f62
Pho <pho.dmc5@gmail.com>
2023-07-26 21:37:43 +0000

    concatenate n elements from source

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates n number of elements
+* from string src to dest
+* @n: number of elements to concatenate
+* @dest: destination string
+* @src: source string
+* Return: char pointer variable
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_end;
+	int src_start;
+
+	dest_end = 0;
+	while (dest[dest_end] != '\0')
+	{
+		dest_end++;
+	}
+
+	for (src_start = 0; src_start < n; src_start++)
+	{
+		dest[dest_end] = src[src_start];
+		dest_end++;
+	}
+
+	dest[dest_end] = '\0';
+
+	return (dest);
+}
