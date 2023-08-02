1cd07e31aadd049d7911f048afdf66dcdc049e8f
mstrchf <phonum5@gmail.com>
2023-08-02 14:18:42 +0000

    find substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - finds first occurence of substring needle in haystack
+ * @needle: substring to find
+ * @haystack: string to search for substring
+ * Return: pointer - beginning of found substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	int match;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+
+		}
+	}
+
+}
