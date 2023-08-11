b66381e30a345040867410fd4c7b976601afe470
Pho <pho.dmc5@gmail.com>
2023-08-11 01:41:41 +0000

    allocate mem for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - allocate memory for an array
+* @nmemb: number of element the array will hold
+* @size: number of bytes by each element
+* Return: void pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+	char *byte_ptr;
+	unsigned int i, total;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	total = nmemb * size;
+	array = malloc(total);
+	byte_ptr = (char *)array;
+
+	for (i = 0; i < total; i++)
+		byte_ptr[i] = 0;
+
+	return (array);
+}
