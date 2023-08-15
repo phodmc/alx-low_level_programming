f626ee55f1366dd7b49141e190934422c46644e2
mstrchf <phonum5@gmail.com>
2023-08-15 02:52:11 +0000

    copy name and owner strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,11 +23,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = (char *) malloc(owner_len + 1);
 
 	if (!new_dog->name || !new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
+	}
 
-	_strcpy(new_dog->name, name);
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
-	_strcpy(new_dog->owner, owner);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
