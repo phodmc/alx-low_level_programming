6e65b00c652d4ef69cf332f0a4824acd97149f14
mstrchf <phonum5@gmail.com>
2023-08-15 02:39:11 +0000

    allocate mem for name and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,6 +23,8 @@ struct dog
 typedef struct dog dog_t;
 
 /* function prototypes */
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
