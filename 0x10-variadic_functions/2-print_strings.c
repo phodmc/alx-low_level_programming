309257111d2ce4df312e5c99852455bbd710fe69
mstrchf <phonum5@gmail.com>
2023-08-19 03:24:24 +0000

    minor fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,6 +11,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *string;
 
+	if (n == 0)
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < n - 1; i++)
