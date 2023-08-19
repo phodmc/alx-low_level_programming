703d68bc3f30ae7cba2a02ea5cc0a90358eaa677
mstrchf <phonum5@gmail.com>
2023-08-19 02:32:41 +0000

    skip separator if null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,18 +10,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (!separator)
-		return;
-
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		if (i + 1 == n)
-			printf("%d\n", va_arg(args, int));
+		if (!separator)
+			printf("%d", va_arg(args, int));
 		else
 			printf("%d%s", va_arg(args, int), separator);
 	}
+
+	printf("%d\n", va_arg(args, int));
 
 	va_end(args);
 }
