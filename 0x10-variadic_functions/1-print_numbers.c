a83d3276b9533e5f8b1614ec908d5b3e198174f4
mstrchf <phonum5@gmail.com>
2023-08-19 02:38:16 +0000

    return when n is 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,6 +10,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
+	if (n == 0)
+		return;
+
 	va_start(args, n);
 
 	for (i = 0; i < n - 1; i++)
