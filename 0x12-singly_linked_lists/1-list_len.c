e318eac68e5cfc591c0d754439b2c71300e92ea9
Pho <pho.dmc5@gmail.com>
2023-08-25 01:59:58 +0000

    count elements in linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* list_len - return number of elements of a linked list
+* @h: head of list
+* Return: number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+		exit(1);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
